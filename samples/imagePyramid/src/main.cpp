2f9edaa00345d4ee9f8778bb1be334369ca03a1a
jadarve <juan.adarve@anu.edu.au>
2018-03-30 23:04:59 -0500

    Refactor image read code.

--- samples/imagePyramid/src/main.cpp
+++ samples/imagePyramid/src/main.cpp
@@ -14,17 +14,56 @@
 #include <vector>
 
 
-void readImage(std::shared_ptr<ll::Session> session, std::shared_ptr<ll::Image> inputImage) {
+using image_t = struct {
+    int32_t width;
+    int32_t height;
+    int32_t channels;
+    std::vector<uint8_t> data;
+};
+
+image_t readImage(const std::string& filepath) {
 
     // read image data
-    int32_t texWidth, texHeight, texChannels;
-    stbi_uc* pixels = stbi_load("dog.jpg", &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
-    uint64_t imageSize = texWidth * texHeight * 4;
+    auto img        = image_t {};
+    stbi_uc* pixels = stbi_load(filepath.c_str(), &img.width, &img.height, &img.channels, STBI_rgb_alpha);
+    img.channels    = 4; // overwrite to 4
+
+    const auto imageSize = static_cast<uint64_t>(img.width * img.height * img.channels);
 
     if (!pixels) {
-        throw std::runtime_error("failed to load texture image!");
+        throw std::runtime_error("failed to read image file: " + filepath);
     }
 
+    img.data.resize(imageSize);
+    std::memcpy(img.data.data(), pixels, imageSize);
+    stbi_image_free(pixels);
+
+    return img;
+}
+
+
+int main() {
+
+    const auto image = readImage("raton.jpg");
+
+    const auto channelType        = ll::ChannelType::Uint8;
+    const auto imageSize          = image.width*image.height*image.channels*ll::getChannelTypeSize(channelType);
+    const auto inputImageMemFlags = vk::MemoryPropertyFlagBits::eDeviceLocal;
+    
+
+    auto session = std::shared_ptr<ll::Session> {ll::Session::create()};
+
+    auto memory = session->createMemory(inputImageMemFlags, imageSize, false);
+
+    auto imgDesc = ll::ImageDescriptor {static_cast<uint32_t>(image.width),
+                                        static_cast<uint32_t>(image.height),
+                                        1,
+                                        static_cast<uint32_t>(image.channels),
+                                        channelType};
+    
+    const auto imgFlags = vk::ImageUsageFlagBits::eStorage | vk::ImageUsageFlagBits::eTransferDst | vk::ImageUsageFlagBits::eTransferSrc;
+
+    auto inputImage = memory->createImage(imgDesc, imgFlags);
 
     // stage buffer
     const auto hostMemFlags = vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent;
@@ -33,46 +72,20 @@ void readImage(std::shared_ptr<ll::Session> session, std::shared_ptr<ll::Image> 
 
     // copy to stage buffer
     auto mapPtr = stageBuffer->map();
-    std::memcpy(mapPtr, pixels, imageSize);
+    std::memcpy(mapPtr, image.data.data(), imageSize);
     stageBuffer->unmap();
-
-    stbi_image_free(pixels);
 
 
     // change input image layout to dst optimal
     // copy stage buffer to input image
     // change input image layout to general
-    auto cmdBuffer = session->createCommandBuffer();
-    cmdBuffer->begin();
-    cmdBuffer->changeImageLayout(*inputImage, vk::ImageLayout::eTransferDstOptimal);
-    cmdBuffer->copyBufferToImage(*stageBuffer, *inputImage);
-    cmdBuffer->changeImageLayout(*inputImage, vk::ImageLayout::eGeneral);
-    cmdBuffer->end();
-    session->run(cmdBuffer);
-}
-
-
-int main() {
-
-    const auto width       = 1280u;
-    const auto height      = 850u;
-    const auto channels    = 4;
-    const auto channelType = ll::ChannelType::Uint8;
-
-    const auto inputImageMemFlags = vk::MemoryPropertyFlagBits::eDeviceLocal;
-
-    const auto memPageSize = width*height*channels*ll::getChannelTypeSize(channelType);
-
-    auto session = std::shared_ptr<ll::Session> {ll::Session::create()};
-
-    auto memory = session->createMemory(inputImageMemFlags, memPageSize, false);
-
-    auto imgDesc = ll::ImageDescriptor {width, height, 1, channels, channelType};
-    const auto imgFlags = vk::ImageUsageFlagBits::eStorage | vk::ImageUsageFlagBits::eTransferDst | vk::ImageUsageFlagBits::eTransferSrc;
-    auto inputImage = memory->createImage(imgDesc, imgFlags);
-
-    // read input image and copy it to inputImage
-    readImage(session, inputImage);
+    auto imgCopyCmdBuffer = session->createCommandBuffer();
+    imgCopyCmdBuffer->begin();
+    imgCopyCmdBuffer->changeImageLayout(*inputImage, vk::ImageLayout::eTransferDstOptimal);
+    imgCopyCmdBuffer->copyBufferToImage(*stageBuffer, *inputImage);
+    imgCopyCmdBuffer->changeImageLayout(*inputImage, vk::ImageLayout::eGeneral);
+    imgCopyCmdBuffer->end();
+    session->run(imgCopyCmdBuffer);
     
 
     auto imagePyramid = ImagePyramid {4};
@@ -88,6 +101,4 @@ int main() {
     session->run(cmdBuffer);
 
     imagePyramid.writeAllImages(session);
-
-    // writeImage(session, inputImage, 0);
 }
