301c44d2b134c81e1ecca253ab072bba4c47e45d
jadarve <juan.adarve@anu.edu.au>
2018-04-12 07:36:23 -0500

    Documentation for Object.h

--- core/include/lluvia/core/Object.h
+++ core/include/lluvia/core/Object.h
@@ -11,15 +11,21 @@
 
 namespace ll {
 
+/**
+ * @brief      Object types.
+ */
 enum class ObjectType : uint32_t {
-    Buffer     = 0,
-    Image      = 1,
-    ImageView  = 2
+    Buffer     = 0,     /**< value for ll::Buffer. */
+    Image      = 1,     /**< value for ll::Image. */
+    ImageView  = 2      /**< value for ll::ImageView. */
 };
 
 
 namespace impl {
 
+    /**
+     * Object type string values used for converting ll::ObjectType to std::string and vice-versa.
+     */
     constexpr const std::array<const char*, 3> ObjectTypeStrings {{
         "BUFFER",
         "IMAGE",
@@ -29,12 +35,36 @@ namespace impl {
 } // namespace impl
 
 
+/**
+ * @brief      Converts from ll::ObjectType enum value to std::string.
+ *
+ * @param[in]  value      
+ *
+ * @tparam     T          function return type. Defaults to std::string.
+ *
+ * @return     Returns the corresponding `std::string` in ll::impl::ObjectTypeStrings for the enum value.
+ */
 template<typename T = std::string>
 inline T objectTypeToString(ll::ObjectType&& value) {
     return impl::enumToString<ll::ObjectType, ll::impl::ObjectTypeStrings.size(), ll::impl::ObjectTypeStrings>(std::forward<ll::ObjectType>(value));
 }
 
 
+/**
+ * @brief      Converts from a string-like object to ll::ObjectType enum.
+ *
+ * This function can be used either with string literas, const char* or `std::string` objects.
+ * \p stringValue parameter is compared against the values in ll::ObjectTypeStrings and the
+ * corresponding enum value is returned. The comparison is case sensitive.
+ *
+ * @param[in]  stringValue  string-like parameter. String literals and `std::string` objects are allowes.
+ *
+ * @tparam     T            \p stringValue type. \p T must satisfies `std::is_convertible<T, std::string>()`
+ *
+ * @return     ll::ObjectType value corresponding to stringValue
+ * 
+ * @throws std::out_of_range if \p stringValue is not found in ll::ObjectTypeStrings.
+ */
 template<typename T>
 inline ll::ObjectType stringToObjectType(T&& stringValue) {
     return impl::stringToEnum<ll::ObjectType, T, ll::impl::ObjectTypeStrings.size(), ll::impl::ObjectTypeStrings>(std::forward<T>(stringValue));
