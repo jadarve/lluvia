62bdc3c52cf672efe675479e94a2a4c4d7382f7f
jadarve <juan.adarve@anu.edu.au>
2018-04-25 21:50:04 -0500

    Documented Object.h

--- core/include/lluvia/core/Object.h
+++ core/include/lluvia/core/Object.h
@@ -1,3 +1,10 @@
+/**
+@file       Object.h
+@brief      Object class and related enums.
+@copyright  2018, Juan David Adarve Bermudez. See AUTHORS for more details.
+            Distributed under the Apache-2 license, see LICENSE for more details.
+*/
+
 #ifndef LLUVIA_CORE_OBJECT_H_
 #define LLUVIA_CORE_OBJECT_H_
 
@@ -12,8 +19,12 @@
 namespace ll {
 
 /**
- * @brief      Object types.
- */
+@brief      Object types.
+
+Each value represent one of the ll::Object child classes.
+
+@sa ll::impl::ObjectTypeStrings string values for this enum.
+*/
 enum class ObjectType : uint32_t {
     Buffer     = 0,     /**< value for ll::Buffer. */
     Image      = 1,     /**< value for ll::Image. */
@@ -24,9 +35,11 @@ enum class ObjectType : uint32_t {
 namespace impl {
 
     /**
-     * Object type string values used for converting ll::ObjectType to std::string and vice-versa.
-     */
-    constexpr const std::array<const char*, 3> ObjectTypeStrings {{
+    @brief Object type string values used for converting ll::ObjectType to std::string and vice-versa.
+
+    @sa ll::ObjectType enum values for this array.
+    */
+    constexpr const std::array<const char*, 3> ObjectTypeStrings{{
         "BUFFER",
         "IMAGE",
         "IMAGE_VIEW"
@@ -36,14 +49,14 @@ namespace impl {
 
 
 /**
- * @brief      Converts from ll::ObjectType enum value to std::string.
- *
- * @param[in]  value      
- *
- * @tparam     T          function return type. Defaults to std::string.
- *
- * @return     Returns the corresponding `std::string` in ll::impl::ObjectTypeStrings for the enum value.
- */
+@brief      Converts from ll::ObjectType enum value to std::string.
+
+@param[in]  value      
+
+@tparam     T          function return type. Defaults to std::string.
+
+@return     Returns the corresponding `std::string` in ll::impl::ObjectTypeStrings for the enum value.
+*/
 template<typename T = std::string>
 inline T objectTypeToString(ll::ObjectType&& value) {
     return impl::enumToString<ll::ObjectType, ll::impl::ObjectTypeStrings.size(), ll::impl::ObjectTypeStrings>(std::forward<ll::ObjectType>(value));
@@ -51,38 +64,99 @@ inline T objectTypeToString(ll::ObjectType&& value) {
 
 
 /**
- * @brief      Converts from a string-like object to ll::ObjectType enum.
- *
- * This function can be used either with string literas, const char* or `std::string` objects.
- * \p stringValue parameter is compared against the values in ll::ObjectTypeStrings and the
- * corresponding enum value is returned. The comparison is case sensitive.
- *
- * @param[in]  stringValue  string-like parameter. String literals and `std::string` objects are allowes.
- *
- * @tparam     T            \p stringValue type. \p T must satisfies `std::is_convertible<T, std::string>()`
- *
- * @return     ll::ObjectType value corresponding to stringValue
- * 
- * @throws std::out_of_range if \p stringValue is not found in ll::ObjectTypeStrings.
- */
+@brief      Converts from a string-like object to ll::ObjectType enum.
+
+This function can be used either with string literals, const char* or `std::string` objects.
+\p stringValue parameter is compared against the values in ll::ObjectTypeStrings and the
+corresponding enum value is returned. The comparison is case sensitive.
+
+@param[in]  stringValue  string-like parameter. String literals and `std::string` objects are allowed.
+
+@tparam     T            \p stringValue type. \p T must satisfies `std::is_convertible<T, std::string>()`
+
+@return     ll::ObjectType value corresponding to stringValue
+ 
+@throws std::out_of_range if \p stringValue is not found in ll::ObjectTypeStrings.
+*/
 template<typename T>
 inline ll::ObjectType stringToObjectType(T&& stringValue) {
     return impl::stringToEnum<ll::ObjectType, T, ll::impl::ObjectTypeStrings.size(), ll::impl::ObjectTypeStrings>(std::forward<T>(stringValue));
 }
 
 
+/**
+@brief Base class for all types that can be used in computer shaders.
+
+Handling of objects in the engine is done through instances allocated in the heap and referenced
+through pointers. The reason is that normal object operations such as copying or moving are hard
+to define for classes that contain some Vulkan resource such as buffers or images.
+ 
+*/
 class Object {
 
 public:
+    /**
+    @brief      Default constructor.
+
+    This constructor needs to be kept in order to construct the child classes. However, this class
+    has no members to initialize.
+    */
     Object()              = default;
-    Object(const Object&) = default;
-    Object(Object&&)      = default;
+
+
+    /**
+    @brief      Copy constructor.
+
+    The copy constructor is deleted intentionally.
+    Copying objects that handle some Vulkan resource
+    such as buffers or images is not well defined.
+    */
+    Object(const Object&) = delete;
+
+
+    /**
+    @brief      Move constructor.
+
+    The move constructor is deleted intentionally.
+    Moving objects that handle Vulkan resources is not well defined.
+    */
+    Object(Object&&)      = delete;
+
 
     virtual ~Object()     = default;
 
-    Object& operator = (const Object&) = default;
-    Object& operator = (Object&&)      = default;
+    /**
+    @brief      Copy assignment.
 
+    Copy assignment is not allowed for Object instances.
+    */
+    Object& operator = (const Object&) = delete;
+
+
+    /**
+    @brief      Move assignment.
+
+    Move assignment is not allowed for Object instances.
+    */
+    Object& operator = (Object&&)      = delete;
+
+
+    /**
+    @brief      Gets the object type.
+
+    This method can be used for safely down-casting pointers of ll::Object type
+    to its children.
+
+    @code
+    ll::Object* ptr = ...
+
+    if (ptr->getType() == ll::ObjectType::Buffer) {
+        auto bufferPtr = static_cast<ll::Buffer*>(ptr);
+    }
+    @endcode
+    
+    @return     The object type.
+    */
     virtual ll::ObjectType getType() const noexcept = 0;
 };
 
