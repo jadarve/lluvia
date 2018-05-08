2960e2ae5e0bb9b013b5971f7e8f1f6751258f47
jadarve <juan.adarve@anu.edu.au>
2018-05-07 21:36:29 -0500

    Documented Program.h

--- core/include/lluvia/core/Program.h
+++ core/include/lluvia/core/Program.h
@@ -1,3 +1,10 @@
+/**
+@file       Program.h
+@brief      Program class.
+@copyright  2018, Juan David Adarve Bermudez. See AUTHORS for more details.
+            Distributed under the Apache-2 license, see LICENSE for more details.
+*/
+
 #ifndef LLUVIA_CORE_PROGRAM_H_
 #define LLUVIA_CORE_PROGRAM_H_
 
@@ -13,12 +20,31 @@ namespace ll {
 class Visitor;
 
 
+/**
+@brief      Class representing Vulkan shader modules in SPIR-V representation.
+
+Program objects hold the SPIR-V code needed to run shader programs in the device. They
+are created using Session::createProgram methods.
+
+@code
+    auto session = ll::Session::create();
+
+    auto program = session->createProgram("path_to_spir-v.spv");
+@endcode
+*/
 class Program {
 
 public:
     Program()                          = delete;
     Program(const Program& program)    = delete;
     Program(Program&& program)         = delete;
+
+    /**
+    @brief      Constructs the object from a Vulkan device and SPIR-V code.
+    
+    @param[in]  device     The Vulkan device.
+    @param[in]  spirvCode  The SPIR-V code.
+    */
     Program(const vk::Device& device, const std::vector<uint8_t>& spirvCode);
 
     ~Program();
@@ -26,10 +52,28 @@ public:
     Program& operator = (const Program& program) = delete;
     Program& operator = (Program&& program)      = delete;
 
+
+    /**
+    @brief      Gets the corresponding Vulkan shader module for this program object.
+    
+    @return     The shader module.
+    */
     vk::ShaderModule getShaderModule() const noexcept;
 
+
+    /**
+    @brief      Gets the SPIR-V code of this object.
+    
+    @return     The SPIR-V code.
+    */
     const std::vector<uint8_t>& getSpirV() const noexcept;
 
+
+    /**
+    @brief      Accepts a visitor to this program.
+    
+    @param      visitor  The visitor
+    */
     void accept(ll::Visitor* visitor);
 
 private:
--- core/src/Program.cpp
+++ core/src/Program.cpp
@@ -1,3 +1,10 @@
+/**
+@file       Program.cpp
+@brief      Program class.
+@copyright  2018, Juan David Adarve Bermudez. See AUTHORS for more details.
+            Distributed under the Apache-2 license, see LICENSE for more details.
+*/
+
 #include "lluvia/core/Program.h"
 #include "lluvia/core/Visitor.h"
 
