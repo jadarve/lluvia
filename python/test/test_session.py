5ae59b912f03df1110547aee7f5e63c7ba021b2a
jadarve <juan.adarve@anu.edu.au>
2018-07-08 12:55:34 -0500

    Added tests for Session.

--- python/test/test_session.py
+++ python/test/test_session.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('../build/python/lib.linux-x86_64-2.7')
+
 import pytest
 import lluvia as ll
 
@@ -5,8 +8,34 @@ import lluvia as ll
 def test_session():
     
     session = ll.Session()
-    memory = session.createMemory()
+    
 
-    print(memory.pageSize)    
+def test_memoryFlags():
+    """
+    Test that the returned memory flags meet the Vulkan speficiation.
 
-    print('moni moni moni')
+    From the Vulkan specification:
+    
+        There must be at least one memory type with both the 
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT and 
+        VK_MEMORY_PROPERTY_HOST_COHERENT_BIT bits set in its propertyFlags.
+        There must be at least one memory type with the 
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT bit set in its propertyFlags.
+    """
+
+    session = ll.Session()
+    memFlags = session.getSupportedMemoryPropertyFlags()
+
+    devFlagsFound = False
+    hostFlagsFound = False
+
+    for flags in memFlags:
+
+        if 'HostVisible' in flags and 'HostCoherent' in flags:
+            hostFlagsFound = True
+
+        if 'DeviceLocal' in flags:
+            devFlagsFound = True
+
+
+    assert(hostFlagsFound and devFlagsFound)
