f89260f9517ac76179f70307ab01eb08b0494721
jadarve <juanda0718@gmail.com>
2019-08-14 15:52:47 -0500

    Started adding Lua library.

--- core/src/Interpreter.cpp
+++ core/src/Interpreter.cpp
@@ -7,10 +7,33 @@
 
 #include "lluvia/core/Interpreter.h"
 
+#include "lluvia/core/ComputeNodeDescriptor.h"
+#include "lluvia/core/types.h"
+
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-lambda-capture"
+
 #define SOL_ALL_SAFETIES_ON 1
 #include "sol/sol.hpp"
 
+#pragma clang diagnostic pop
+
 namespace ll {
+
+
+void registerTypes(sol::table& lib) {
+
+    lib.new_usertype<ll::vec3ui>("vec3ui",
+        "x", &ll::vec3ui::x,
+        "y", &ll::vec3ui::y,
+        "z", &ll::vec3ui::z);
+
+    lib.new_usertype<ll::ComputeNodeDescriptor>("ComputeNodeDescriptor",
+        "functionName", sol::property(&ll::ComputeNodeDescriptor::getFunctionName, &ll::ComputeNodeDescriptor::setFunctionName)
+        );
+}
+
 
 Interpreter::Interpreter() :
     m_lua {std::make_unique<sol::state>()} {
@@ -18,7 +41,10 @@ Interpreter::Interpreter() :
     // load default libraries
     m_lua->open_libraries(sol::lib::base, sol::lib::math);
 
-    // TODO: register types
+    // library namespace
+    auto lib = (*m_lua)["ll"].get_or_create<sol::table>();
+
+    registerTypes(lib);
 }
 
 
