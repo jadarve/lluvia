16c1546911795f7130519b2d4d029d2e6e34a5cb
jadarve <juanda0718@gmail.com>
2019-08-22 16:05:09 -0500

    Added ImagePyramid.lua

---
+++ lua/ImagePyramid.lua
@@ -0,0 +1,10 @@
+local builder = ll.class(ll.ContainerNodeBuilder)
+
+-- Input parameters
+-- * Pyramid levels
+
+-- Need to declare ports:: ContainerNodeDescriptor?
+-- bind
+-- record
+
+ll.registerNodeBuilder('imagePyramid', builder)
