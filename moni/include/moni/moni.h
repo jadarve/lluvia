86cf7bcf44644466bcf97f999862e5116c1e168a
Juan David Adarve <juanda0718@gmail.com>
2020-01-02 21:25:13 -0500

    Deleted test example.

--- moni/include/moni/moni.h
+++
@@ -1,8 +0,0 @@
-#ifndef MONI_MONI_H_
-#define MONI_MONI_H_
-
-namespace moni {
-    void sayHello();
-} // namespace moni
-
-#endif // MONI_MONI_H_
--- moni/python/src/moni/moni.pyx
+++
@@ -1,3 +0,0 @@
-
-def sayMoni():
-    print('moni moni')
