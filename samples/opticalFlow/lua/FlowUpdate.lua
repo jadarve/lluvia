35cc123ef2f2c4f19740993dc19d57d0d94406a7
Juan David Adarve <juanda0718@gmail.com>
2020-02-04 23:10:49 -0500

    Added FlowUpdate. Need to test.

---
+++ samples/opticalFlow/lua/FlowUpdate.lua
@@ -0,0 +1,53 @@
+local builder = ll.class(ll.ComputeNodeBuilder)
+
+function builder.newDescriptor() 
+    
+    local desc = ll.ComputeNodeDescriptor.new()
+    
+    desc.builderName  = 'FlowUpdate'
+    desc.localShape   = ll.vec3ui.new(32, 32, 1)
+    desc.gridShape    = ll.vec3ui.new(1, 1, 1)
+    desc.program      = ll.getProgram('FlowUpdate')
+    desc.functionName = 'main'
+
+    desc:addPort(ll.Portdescriptor.new(0, 'in_gray', ll.PortDirection.In, ll.PortType.ImageView))
+    desc:addPort(ll.Portdescriptor.new(1, 'in_gradient', ll.PortDirection.In, ll.PortType.ImageView))
+    desc:addPort(ll.Portdescriptor.new(2, 'in_gray_old', ll.PortDirection.In, ll.PortType.ImageView))
+    desc:addPort(ll.Portdescriptor.new(3, 'in_flow', ll.PortDirection.In, ll.PortType.ImageView))
+
+    desc:addPort(ll.Portdescriptor.new(4, 'out_gray', ll.PortDirection.Out, ll.PortType.ImageView))
+    desc:addPort(ll.Portdescriptor.new(5, 'out_flow', ll.PortDirection.Out, ll.PortType.ImageView))
+
+    return desc
+end
+
+function builder.onNodeInit(node)
+    ll.logd('FlowUpdate', 'onNodeInit')
+
+    in_flow = node:getPort('in_flow')
+    in_gray = node:getPort('in_gray')
+
+    -- ll::Memory where out_flow will be allocated
+    memory = in_flow.memory
+
+    out_flow = memory:createImageView(in_flow.imageDescriptor, in_flow.descriptor)
+    out_gray = memory:createImageView(in_gray.imageDescriptor, in_gray.descriptor)
+
+    -- need to change image layout before binding
+    out_flow:changeImageLayout(ll.ImageLayout.General)
+    out_gray:changeImageLayout(ll.ImageLayout.General)
+
+    node:bind('out_flow', out_flow)
+    node:bind('out_gray', out_gray)
+
+    ll.logd('FlowUpdate', 'in_gray ', string.format('[%d, %d, %d]', in_gray.width, in_gray.height, in_gray.channelCount)
+                              , 'out_gray', string.format('[%d, %d, %d]', out_gray.width, out_gray.height, out_gray.channelCount))
+    ll.logd('FlowUpdate', 'in_flow ', string.format('[%d, %d, %d]', in_flow.width, in_flow.height, in_flow.channelCount)
+                              , 'out_flow', string.format('[%d, %d, %d]', out_flow.width, out_flow.height, out_flow.channelCount))
+    
+    node:configureGridShape(ll.vec3ui.new(out_flow.width, out_flow.height, 1))
+
+    ll.logd('FlowUpdate', 'onNodeInit: finish')
+end
+
+ll.registerNodeBuilder('FlowUpdate', builder)
