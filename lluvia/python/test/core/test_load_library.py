18875958ade8cb43b93c45a3f05ce28f5a4513b8
Juan David Adarve <juanda0718@gmail.com>
2020-08-09 19:09:32 -0500

    Added Python test.

---
+++ lluvia/python/test/core/test_load_library.py
@@ -0,0 +1,29 @@
+
+import pytest
+
+
+def test_load_library():
+
+    import lluvia as ll
+
+    print("LL_PATH: ", ll.__path__)
+
+    session = ll.createSession()
+    session.loadLibrary('lluvia/cpp/core/test/nodes/test_node_library.zip')
+
+    desc = session.createComputeNodeDescriptor('nodes/Assign')
+    assert(desc != None)
+
+    program = session.getProgram('nodes/Assign')
+    assert(program != None)
+
+    node = session.createComputeNode("nodes/Assign")
+    assert(node != None)
+
+
+if __name__ == "__main__":
+
+    from config import add_lluvia_import_path
+    add_lluvia_import_path()
+
+    raise SystemExit(pytest.main([__file__]))
