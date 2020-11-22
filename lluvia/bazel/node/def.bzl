5bf10c729c8a45315075674ef5fdecb403c97f06
jadarve <juanda0718@gmail.com>
2020-11-22 09:55:18 -0500

    Added @lluvia to glsl targets.

--- lluvia/bazel/node/def.bzl
+++ lluvia/bazel/node/def.bzl
@@ -1,7 +1,7 @@
 """
 """
 
-load("//lluvia/bazel/node:macros.bzl",
+load("@lluvia//lluvia/bazel/node:macros.bzl",
     _ll_node = "ll_node",
     _ll_node_library = "ll_node_library")
 
---
+++ lluvia/nodes/lluvia/RGBA2Gray.lua
@@ -0,0 +1,41 @@
+local builder = ll.class(ll.ComputeNodeBuilder)
+
+function builder.newDescriptor()
+
+    local desc = ll.ComputeNodeDescriptor.new()
+
+    desc.builderName  = 'lluvia/RGBA2Gray'
+    desc.localShape   = ll.vec3ui.new(32, 32, 1)
+    desc.gridShape    = ll.vec3ui.new(1, 1, 1)
+    desc.program      = ll.getProgram('lluvia/RGBA2Gray')
+    desc.functionName = 'main'
+
+    desc:addPort(ll.PortDescriptor.new(0, 'in_rgba', ll.PortDirection.In, ll.PortType.ImageView))
+    desc:addPort(ll.PortDescriptor.new(1, 'out_gray', ll.PortDirection.Out, ll.PortType.ImageView))
+
+    return desc
+end
+
+function builder.onNodeInit(node)
+
+    local in_RGBA = node:getPort('in_rgba')
+
+    -- ll::Memory where out_gray will be allocated
+    local memory = in_RGBA.memory
+
+    -- out_gray descriptors
+    local imgDesc = ll.ImageDescriptor.new(in_RGBA.imageDescriptor)
+    imgDesc.channelCount = 1
+
+    local imgViewDesc = ll.ImageViewDescriptor.new(in_RGBA.descriptor)
+
+    local out_gray = memory:createImageView(imgDesc, imgViewDesc)
+
+    -- need to change image layout before binding
+    out_gray:changeImageLayout(ll.ImageLayout.General)
+
+    node:bind('out_gray', out_gray)
+    node:configureGridShape(ll.vec3ui.new(out_gray.width, out_gray.height, 1))
+end
+
+ll.registerNodeBuilder('lluvia/RGBA2Gray', builder)
