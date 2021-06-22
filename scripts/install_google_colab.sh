07ffaffda69c1907a2be50de02605edaa8ddf0a3
Juan David Adarve <juanda0718@gmail.com>
2021-06-21 22:34:20 -0500

    Run apt update once

--- scripts/install_google_colab.sh
+++ scripts/install_google_colab.sh
@@ -19,15 +19,15 @@
 # Vulkan SDK
 wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc | apt-key add -
 wget -qO /etc/apt/sources.list.d/lunarg-vulkan-1.2.176-bionic.list https://packages.lunarg.com/vulkan/1.2.176/lunarg-vulkan-1.2.176-bionic.list
-apt update
-apt install -y vulkan-sdk
 
 # Bazel
 apt install apt-transport-https curl gnupg
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
 mv bazel.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-apt update && apt install -y bazel
+
+apt update
+apt install -y vulkan-sdk bazel
 
 # Go to lluvia's directory
 cd lluvia
