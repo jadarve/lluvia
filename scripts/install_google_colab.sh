1eab71affe41de540324d0d64bc15e34804c14dc
Juan David Adarve <juanda0718@gmail.com>
2021-06-21 22:27:02 -0500

    Some fixes.

--- scripts/install_google_colab.sh
+++ scripts/install_google_colab.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# This script installs lluvia Python package on a Google colab environment.
+#
+# Usage:
+#
+# Make sure the environment has a GPU enabled.
+#
+# In a cell, paste the following two statements
+#
+#    !git clone https://github.com/jadarve/lluvia.git
+#    !./lluvia/scripts/install_google_colab.sh
+#
+# After that, import lluvia in a cell as:
+#
+#    import lluvia as ll
+
+
 # Vulkan SDK
 wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc | apt-key add -
 wget -qO /etc/apt/sources.list.d/lunarg-vulkan-1.2.176-bionic.list https://packages.lunarg.com/vulkan/1.2.176/lunarg-vulkan-1.2.176-bionic.list
@@ -13,8 +29,11 @@ mv bazel.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
 apt update && apt install -y bazel
 
+# Go to lluvia's directory
+cd lluvia
+
 # Build
-bazel build //lluvia/python:lluvia_wheel
+bazel build --curses=no //lluvia/python:lluvia_wheel
 
 # Install
 pip3 install lluvia/bazel-bin/lluvia/python/lluvia-0.0.1-py3-none-any.whl
