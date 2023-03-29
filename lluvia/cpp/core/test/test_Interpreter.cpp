0c1df36cea340db4ae969c09f16068ab35cebcbd
Juan David Adarve <juanda0718@gmail.com>
2023-03-28 21:25:24 -0500

    Added test for map and set from base64

--- lluvia/cpp/core/test/test_Interpreter.cpp
+++ lluvia/cpp/core/test/test_Interpreter.cpp
@@ -84,3 +84,76 @@ TEST_CASE("test_non_existing_file", "test_Interpreter")
     REQUIRE_THROWS_AS(session->scriptFile("some_invalid_path.lua"), std::system_error);
     REQUIRE_FALSE(session->hasReceivedVulkanWarningMessages());
 }
+
+TEST_CASE("bufferMapAndSetFromBase64", "test_Interpreter")
+{
+
+    ///////////////////////////////////////////////////////
+    // given a session and a Lua script
+
+    auto session = ll::Session::create(ll::SessionDescriptor().enableDebug(true));
+    REQUIRE(session != nullptr);
+
+    constexpr auto lua = R"(
+
+        local builder = ll.class(ll.ContainerNodeBuilder)
+        builder.name = 'MapAndSetFromBase64'
+        builder.doc = 'doc string'
+
+        function builder.newDescriptor()
+
+            local desc = ll.ContainerNodeDescriptor.new()
+
+            desc.builderName = builder.name
+
+            return desc
+        end
+
+        function builder.onNodeInit(node)
+
+            local base64 = 'T3mZ8YSxEOdpxg=='
+            local decodedData = ll.fromBase64(base64)
+            local bufferSize = decodedData:size()
+
+            local hostMemory = ll.getHostMemory()
+            local stagingBuffer = hostMemory:createBuffer(bufferSize)
+            stagingBuffer:mapAndSetFromVectorUint8(decodedData)
+
+            node:bind('stagingBuffer', stagingBuffer)
+        end
+
+        function builder.onNodeRecord(node, cmdBuffer)
+            -- do nothing
+        end
+
+        ll.registerNodeBuilder(builder)
+    )";
+
+    const auto data = std::vector<uint8_t> {79, 121, 153, 241, 132, 177, 16, 231, 105, 198};
+    REQUIRE_NOTHROW(session->script(lua));
+
+    ///////////////////////////////////////////////////////
+    // when a container node is created
+    auto node = session->createContainerNode("MapAndSetFromBase64");
+    REQUIRE(node != nullptr);
+
+    // and the node is initialized
+    REQUIRE_NOTHROW(node->init());
+
+    ///////////////////////////////////////////////////////
+    // then the staging buffer is retrieved
+    auto stagingBuffer = std::static_pointer_cast<ll::Buffer>(node->getPort("stagingBuffer"));
+    REQUIRE(stagingBuffer != nullptr);
+
+    // and the content of the staging buffer is the same as the original data
+    {
+        auto mapPtr = stagingBuffer->map<uint8_t[]>();
+        REQUIRE(mapPtr != nullptr);
+
+        for (auto i = 0u; i < data.size(); ++i) {
+            REQUIRE(mapPtr[i] == data[i]);
+        }
+    }
+
+    REQUIRE_FALSE(session->hasReceivedVulkanWarningMessages());
+}
